5c79a32d860348b813c6751eba33c5940f5880e9
artemkosnikov <Boxberry123>
2021-12-16 15:46:16 +0300

    2-nd commit

--- server.js
+++ server.js
@@ -15,33 +15,33 @@ app.use(async (ctx) => {
         'Access-Control-Allow': '*'
     })
 })
-app.use(async (ctx, next) => {
-    const origin = ctx.request.get('Origin');
-    if (!origin) {
-        return await next();
-    }
-    const headers = {'Access-Control-Allow-Origin': '*', };
-
-    if (ctx.request.method !== 'OPTIONS') {
-        ctx.response.set({...headers});
-        try {
-            return await next();
-        } catch (e) {
-            e.headers = {...e.headers, ...headers};
-            throw e;
-        }
-    }
-
-    if (ctx.request.get('Access-Control-Request-Method')) {
-        ctx.response.set({
-            ...headers,
-            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH',
-        });
-        if (ctx.request.get('Access-Control-Request-Headers')) {
-            ctx.response.set('Access-Control-Allow-Headers', ctx.request.get('Access-Control-Allow-Request-Headers'));
-        }
-        ctx.response.status = 204;
-    }
-})
+// app.use(async (ctx, next) => {
+//     const origin = ctx.request.get('Origin');
+//     if (!origin) {
+//         return await next();
+//     }
+//     const headers = {'Access-Control-Allow-Origin': '*', };
+//
+//     if (ctx.request.method !== 'OPTIONS') {
+//         ctx.response.set({...headers});
+//         try {
+//             return await next();
+//         } catch (e) {
+//             e.headers = {...e.headers, ...headers};
+//             throw e;
+//         }
+//     }
+//
+//     if (ctx.request.get('Access-Control-Request-Method')) {
+//         ctx.response.set({
+//             ...headers,
+//             'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH',
+//         });
+//         if (ctx.request.get('Access-Control-Request-Headers')) {
+//             ctx.response.set('Access-Control-Allow-Headers', ctx.request.get('Access-Control-Allow-Request-Headers'));
+//         }
+//         ctx.response.status = 204;
+//     }
+// })
 const port = process.env.PORT || 7070;
 const server = http.createServer(app.callback()).listen(port);
